bf6dbd16674dcb435ccd3308fb9d03e87bc82b5c
thiagobachiega <thiagobachiega1@gmail.com>
2022-06-06 10:41:09 -0300

    ex:1001 a 1002

---
+++ 1001-extremely-basic/index.js
@@ -0,0 +1,5 @@
+const input = ["10","9"]
+
+var result = Number(lines[0]) + Number(lines[1]);
+
+console.log('X = ' + result);
---
+++ 1002-area-of-a-circle/index.js
@@ -0,0 +1,7 @@
+const input = ["2.00","100.64","150.00"]
+
+const circuference = 3.14159
+
+const area = Number(circuference) * (Number(input[1]) * Number(input[1]))
+
+console.log('A = ' + area)
